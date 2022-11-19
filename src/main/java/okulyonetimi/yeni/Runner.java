ae84cfce9ae8972c9bbfe2e959dff909542e925b
Muhammet Okumus <m.okumus07@gmail.com>
2022-11-19 16:36:25 +0300

    ad

---
+++ src/main/java/okulyonetimi/yeni/Runner.java
@@ -0,0 +1,13 @@
+package okulyonetimi.yeni;
+
+import okulyonetimi.OkulAnasayfa;
+
+public class Runner {
+
+
+    public static void main(String[] args) {
+
+        OkulAnasayfa.okulAnaSayfasi();
+
+    }
+}
